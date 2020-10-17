6f03e96a0e50712ccc62fdc0084c4d56698097f8
Jagan R <37062981+jaganchoccy@users.noreply.github.com>
2020-10-17 11:20:03 +0530

    first commt

---
+++ xploreDev/lib/widgets/logIn.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: 
+    );
+  }
+}
